f19ea2473f984b03f72f197efefbafc8af59ba1d
mshita <t.mshita118@gmail.com>
2024-12-13 13:28:06 +0000

    フォローボタンの設置

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.timeline, name='timeline'),
+    path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
+    path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
 ]
--- timeline/views.py
+++ timeline/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import Post
+from .models import Follow
 from .forms import PostForm
 
-
+#タイムライン
 @login_required
 def timeline(request):
-    posts = Post.objects.all().order_by('-created_at')
+    followed_users = Follow.objects.filter(
+        follower=request.user).values('followed')
+    posts = Post.objects.filter(user__in=followed_users).order_by('-created_at')
     form = PostForm()
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -16,3 +20,24 @@ def timeline(request):
             post.save()
             return redirect('timeline')
     return render(request, 'timeline/timeline.html', {'posts': posts, 'form': form})
+
+#フォロー機能
+@login_required
+
+def follow_user(request, user_id):
+    followed_user = User.objects.get(id=user_id)
+
+    if not Follow.objects.filter(followe=request.user, followed=followed_user).exists():
+        Follow.objects.create(follower=request.user, followed=followed_user)
+
+    return redirect('timeline')
+
+@login_required
+def unfollow_user(request, user_id):
+    followed_user = User.ubjects.get(id=user_id)
+
+    follow = Follow.objects.filter(follower=request.user, followed=followed_user)
+    if follow.exists():
+        follow.delete()
+
+    return redirect('timeline')
