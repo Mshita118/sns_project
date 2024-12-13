209b9c15a8215dbb73ff5850b092c57e1e7c1df9
mshita <t.mshita118@gmail.com>
2024-12-13 13:11:40 +0000

    フォロー機能のモデル追加

--- timeline/models.py
+++ timeline/models.py
@@ -9,4 +9,16 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.user.username}: {self.content[:20]}"
-    
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(
+        User, related_name='following', on_delete=models.CASCADE)
+    followed = models.ForeignKey(
+        User, related_name='followers', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('follower', 'followed')
+
+    def __str__(self):
+        return f"{self.follower.username } follows {self.followed.username }"
