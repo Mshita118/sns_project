0474c85ba40d4cd27f124fdca5c6a32797b554b8
mshita <t.mshita118@gmail.com>
2024-12-14 13:09:44 +0000

    いいねとコメントモデルの作成

--- timeline/models.py
+++ timeline/models.py
@@ -22,3 +22,20 @@ class Follow(models.Model):
 
     def __str__(self):
         return f"{self.follower.username } follows {self.followed.username }"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['post', 'user'], name='unique_like')
+        ]
