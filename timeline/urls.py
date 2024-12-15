881270aace82e92a9c740226985c8c2bcfab98a4
mshita <t.mshita118@gmail.com>
2024-12-15 12:59:30 +0000

    コメントurlのコーディング

--- timeline/urls.py
+++ timeline/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('follow/<int:user_id>/', views.follow_user, name='follow_user'),
     path('unfollow/<int:user_id>/', views.unfollow_user, name='unfollow_user'),
     path('search/', views.search_posts, name='search_posts'),
+    path('<int:post_id>/', views.post_detail, name='post_detail'),
 ]
