4061de6203ab98c732bcbdf111a990da945ae598
mshita <t.mshita118@gmail.com>
2024-12-16 13:23:28 +0000

    いいねモデルの作成

--- timeline/models.py
+++ timeline/models.py
@@ -22,6 +22,19 @@ class Comment(models.Model):
         return f"Comment by {self.user.username} on {self.post.id}"
 
 
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user.username} likes { self.post.id}"
+
+
 class Follow(models.Model):
     follower = models.ForeignKey(
         User, related_name='following', on_delete=models.CASCADE)
