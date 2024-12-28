90f0b9b95028115c74fa47beb6c3b64967f164e9
mshita <t.mshita118@gmail.com>
2024-12-28 14:38:00 +0000

    モデルテスト完了

--- timeline/tests.py
+++ timeline/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile, Post, Comment, Like, Follow
+# models.pyのテスト
 
-# Create your tests here.
+
+class ProfileModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        self.profile, created = Profile.objects.get_or_create(user=self.user)
+
+    def test_profile_creation(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+        self.assertEqual(self.profile.__str__(), self.user.username)
+
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        self.post = Post.objects.create(
+            user=self.user, content='This is a test post')
+
+    def test_post_creation(self):
+        self.assertTrue(isinstance(self.post, Post))
+        self.assertEqual(self.post.__str__(),
+                         f"{self.user.username}: {self.post.content[:20]}")
+
+
+class CommentModelCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass')
+        self.post = Post.objects.create(
+            user=self.user, content='This is a test post')
+        self.comment = Comment.objects.create(
+            post=self.post, user=self.user, content='This is a test comment')
+
+    def test_comment_creation(self):
+        self.assertTrue(isinstance(self.comment, Comment))
+        self.assertEqual(self.comment.__str__(),
+                         f"Comment by {self.user.username} on {self.post.id}")
